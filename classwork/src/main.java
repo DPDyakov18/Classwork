797d32e193e6954d2ffcf656237b8b527178e835
Dimitar Dyakov <dpdyakov18@codingburgas.bg>
2022-10-20 13:58:23 +0300

    abstract apple

--- classwork/src/main.java
+++ classwork/src/main.java
@@ -1,17 +1,20 @@
 import java.util.*;
-class Ma{
-    String languages;
-    Ma(String lang){
-        languages=lang;
-        System.out.println(languages+" Programming language ");
+
+abstract class Fruits{
+    abstract void taste();
+}
+
+class Apple extends Fruits {
+    @Override
+    void taste(){
+        System.out.println("Sweet taste");
     }
 }
 
 public class main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        Ma m1=new Ma("Java");
-        Ma m2=new Ma("Python");
-        Ma m3=new Ma("C");
+        Apple a = new Apple();
+        a.taste();
     }
 }
